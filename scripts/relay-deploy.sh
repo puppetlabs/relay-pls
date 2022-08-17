084323baea29796b4dc1b0708599acc1c283a0f5
Rick Lane <rick.lane@puppet.com>
2022-08-17 12:35:22 -0700

    Remove outdated CI

--- scripts/relay-deploy.sh
+++
@@ -1,5 +0,0 @@
-# This file contains long-running branch deployment instructions. Once a
-# workflow exists for your target environment, configure this file to point your
-# branch at that workflow.
-
-#RELAY_WORKFLOWS[features/example]=relay-deploy-example-feature-branch
